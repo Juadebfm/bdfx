1bac841ccaccfe9fe21561c02e5b6c29609e44f0
Juadeb <juadebgabriel@gmail.com>
2023-10-17 15:35:30 +0100

    fix

--- app/components/Line.jsx
+++ app/components/Line.jsx
@@ -154,7 +154,7 @@ function LineChart() {
   return (
     <div>
       <h1 className="font-bold text-3xl text-black text-center">
-        Exchange Rates (Months)
+        Exchange Rates For Popular Currencies
       </h1>
       <div
         style={{

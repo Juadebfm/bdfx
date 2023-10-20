bcca6bfe12f410e23a015b871700c7f67583d07c
Juadeb <juadebgabriel@gmail.com>
2023-10-20 16:27:32 +0100

    fix:

--- app/businessnews/page.jsx
+++ app/businessnews/page.jsx
@@ -1,15 +1,12 @@
-"use client"
+"use client";
 
 import React from "react";
-import Markets from "../components/Markets";
-import FeaturedNews from "../components/FeaturedNews";
+
 import CTA from "../components/CTA";
 
 const BusinessNews = () => {
   return (
     <main>
-      <Markets />
-      <FeaturedNews />
       <CTA />
     </main>
   );
